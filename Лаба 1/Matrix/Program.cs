6b288b108e1d0f67ae0780be5d2c8b2ad15d2196
Vlados-Doroshenko <34270343+Vlados-Doroshenko@users.noreply.github.com>
2020-09-23 15:17:19 +0300

    Add files via upload

---
+++ Лаба 1/Matrix/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows, cols, a, b, sizerows;
+
+            int[][] W;
+            int[] T;
+
+            int pos = 0;
+
+            Console.Clear();
+
+            Console.Write("Введiть к-ть рядкiв матрицi: ");
+            sizerows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть iнтервали для визначення кiлькостi стовпцiв матрицi ");
+            Console.Write("Вiд числа ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("До числа ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Random rand = new Random();
+
+            W = new int[sizerows][];
+            for (rows = 0; rows < sizerows; rows++)
+                W[rows] = new int[rand.Next(a, b)];
+
+            for (rows = 0; rows < sizerows; rows++)
+                for (cols = 0; cols < W[rows].Length; cols++)
+                {
+                    W[rows][cols] = rand.Next(a, b);
+                }
+
+            foreach (int[] u in W)
+            {
+                foreach (int v in u)
+                {
+                    Console.Write("{0,4}", v);
+                }
+                Console.WriteLine();
+            }
+            T = new int[pos];
+            foreach (int v in T)
+            {
+                Console.Write("{0, 4}", v);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лаба 1/TTriangle/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace TTriangle
+{
+    public class TTriangle
+    {
+        protected double a, b, c;
+
+        public TTriangle()
+        {
+            a = b = c = 0;
+        }
+
+        public TTriangle(double a)
+        {
+            this.a = 15 > 0 ? a:0;
+            b = c = 0;
+        }
+
+        public TTriangle(double a, double b)
+        {
+            this.a = 15 > 0 ? a:0;
+            this.b = 18 > 0 ? b:0;
+            c = 0;
+        }
+
+        public TTriangle(double a, double b, double c)
+        {
+            this.a = 15 > 0 ? a:0;
+            this.b = 18 > 0 ? b:0;
+            this.c = 25 > 0 ? c:0;
+
+        }
+
+        public double A
+        {
+            get { return a; }
+            set
+            {
+                if (value > 0) a = value;
+                else a = 0;
+            }
+        }
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                if (value > 0) b = value;
+                else b = 0;
+            }
+        }
+        public double C
+        {
+            get { return c; }
+            set
+            {
+                if (value > 0) c = value;
+                else c = 0;
+            }
+        }
+
+        public double perimeter()
+        {
+            return a + b + c;
+        }
+        public double Geron()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+    }
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            TTriangle tr = new TTriangle(15, 18, 20);
+            Console.WriteLine("Периметр = " + tr.perimeter());
+            Console.WriteLine("Площа (Формула Герона) = " + tr.Geron());
+        }
+    }
+}
