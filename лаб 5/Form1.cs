567e9675eb475d8daee20918729dec25876cfbd4
Vlados-Doroshenko <34270343+Vlados-Doroshenko@users.noreply.github.com>
2021-05-15 22:15:49 +0300

    Add files via upload

---
+++ лаб 5/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Figure figure;
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = b;
+            numericUpDown1.Value = 100;
+        }
+
+        private async void circle_Click(object sender, EventArgs e)
+        {
+            figure = new Circle(2, 80, 56);
+            await Task.Factory.StartNew(start_thread_parse);
+        }
+
+        private async void square_Click(object sender, EventArgs e)
+        {
+            figure = new Square(56);
+            await Task.Factory.StartNew(start_thread_parse);
+        }
+
+        private async void rhomb_Click(object sender, EventArgs e)
+        {
+            figure = new Rhomb(10, 180, 50, 50);
+            await Task.Factory.StartNew(start_thread_parse);
+        }
+
+        private void start_thread_parse()
+        {
+            while (true)
+            {
+                figure.MoveRight(pictureBox1);
+                Thread.Sleep(Convert.ToInt32(numericUpDown1.Value));
+                this.Invoke(new MethodInvoker(delegate {
+                    pictureBox1.Refresh();
+                }));
+            }
+        }
+    }
+}
---
+++ лаб 5/Square.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Square : Figure
+    {
+        private double side { get; set; }
+        public Square() { }
+        public Square(int sideLength) { this.side = sideLength; }
+        public Square(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Square(int x, int y, double side)
+        {
+            this.side = side;
+            this.x = x;
+            this.y = y;
+        }
+        public override void DrawBlack(PictureBox p)
+        {
+            using (var g = Graphics.FromImage(p.Image))
+            {
+                g.DrawRectangle(Pens.Black, x, y, (int)side, (int)side);
+            }
+        }
+        public override void HideDrawingBackGround(PictureBox p)
+        {
+            using (var g = Graphics.FromImage(p.Image))
+            {
+                g.DrawRectangle(Pens.White, x, y, (int)side, (int)side);
+            }
+        }
+    }
+}
